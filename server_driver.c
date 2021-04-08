e090101d97e41faf11e942f307cae8c229ba1049
AndrewIngle <drew.ingle@gmail.com>
2021-04-08 12:43:47 -0500

    Server_driver now passes names to servers via fifo

--- server_driver.c
+++ server_driver.c
@@ -1,17 +1,27 @@
 //Andrew Ingle 04/07/2021 - Team I - Final Project
 //Main Driver Programmer for Server, will fork and execute to create servers
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int main() {
    
    printf("\nServer Driver says hello!\n"); //for debugging
 
+   int server_name = 1; //name of first server will be incremented as we go along
+
+   if (mkfifo("myfifo1", 0777)==-1){    //Create Fifo to send server names to child servers
+         printf("Could not create fifo file\n");
+         return 1;
+   }
+  
    //creating 3 servers
    for (int i = 0;i<3;i++){
 
@@ -25,11 +35,30 @@ int main() {
          //server_main program launch
          execlp("./server_main","server_main",NULL); //system call provided by Andrew
       }
+      sleep(1); //to give time for servers to open
    }
+
+   //opening fifo to send names to servers
+   int fd = open("myfifo1", 01); //this waits until child server has opened fifo 
+      if (fd== -1){
+      return 1;
+   }
+
+   //sending names to servers
+   for (int i=0;i<3;i++){
+      write(fd, &server_name, sizeof(int));
+      server_name++; //incrementing name
+   }
+    
+
+
+   
 
    /* parent process */
 	/* parent will wait for the child to complete */
-   sleep(2); //for debugging to give time for servers to print to terminal
+
+   sleep(2); //for debugging to give time for child servers to print to terminal
+   close(fd);
    wait(NULL);
 	printf("All child processes complete, Server Driver will exit\n");
 	
--- server_main.c
+++ server_main.c
@@ -6,8 +6,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
    printf("\nServer %d says hello\n",getpid());
+
+   int fd = open("myfifo1", O_RDONLY); //fifo between manager and assistant
+
+   int server_name;
+   //reading from pipe connected to managaer
+   read(fd,&server_name,sizeof(int));
+
+   printf("\nServer %d is alive and named!\n",server_name);
+   close(fd);
+
    return 0;
 }
