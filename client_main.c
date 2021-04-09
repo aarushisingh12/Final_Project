e80d4a021167848889a29287319ddcee95f676f9
AndrewIngle <drew.ingle@gmail.com>
2021-04-09 17:15:25 -0500

    Updating client_main with server port selection code - still debugging

--- client_main.c
+++ client_main.c
@@ -11,17 +11,34 @@
 #include <netinet/in.h>
 
 int main() {
-   printf("\nClient says hello\n");
+   printf("\nClient says hello\n"); //for debugging
 
-   char client_message[50] = "Client message to server"; //buffer for socket send for testing
-   
+   char client_message[50] = "Client message to server"; //for testing, buffer for socket send for testing
+
+   printf("\nWhich server do you want to connect to (1, 2 or 3): \n");
+
+   int chosenServer;
+   scanf("%d", chosenServer);
+
    //client side socket creation
    int sock;
    sock = socket(AF_INET,SOCK_STREAM,0); // for tcp
    struct sockaddr_in server_address;
    server_address.sin_family = AF_INET;
-   server_address.sin_port = htons(8001); //for local
+
+   //ports need to be appropriate for current OS
+   switch(chosenServer) {
+         case 1:
+            server_address.sin_port = htons(7400); //for local connections
+         case 2:
+            server_address.sin_port = htons(7401); //for local connections
+         case 3:
+            server_address.sin_port = htons(7402); //for local connections
+   }
+
    server_address.sin_addr.s_addr = INADDR_ANY; //for local
+   
+   //server_address.sin_port = htons(8001); //for local on linux, was using for testing
    //previous attempts to get server to work remotely
    //server_address.sin_addr.s_addr = inet_addr("10.203.72.24");
    //inet_pton(AF_INET, "10.203.72.24", &(server_address.sin_addr));
@@ -32,7 +49,7 @@ int main() {
    if (connection == -1){
        printf("There is an issue with connection!!!\n\n");
     }
-    send(sock,client_message,sizeof(client_message),0);
+    send(sock,client_message,sizeof(client_message),0); //a debug test
     sleep(1);
     close(sock);
 
