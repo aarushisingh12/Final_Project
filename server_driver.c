5e6826f3292f63048b688ac5ed3fc1224501a233
aarushisingh12 <71734806+aarushisingh12@users.noreply.github.com>
2021-04-09 16:30:54 -0500

    debugging server/client set up

--- server_driver.c
+++ server_driver.c
@@ -10,36 +10,43 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
 
 int main() {
-   
+
    printf("\nServer Driver says hello!\n"); //for debugging
 
    int server_name = 1; //name of first server will be incremented as we go along
 
-   if (mkfifo("myfifo1", 0777)==-1){    //Create Fifo to send server names to child servers
-         printf("Could not create fifo file\n");
-         return 1;
+   unlink("myfifo1");
+   if (mkfifo("myfifo1", 0666)==-1){    //Create Fifo to send server names to child servers
+      printf("Could not create fifo file\n");
+      perror("mkfifo() failed");
+      return 1;
    }
-  
+
    //creating 3 servers
    for (int i = 0;i<3;i++){
 
       pid_t pid = fork();
-   
+
       if(pid < 0) {
          fprintf(stderr, "fork failed for next server.");
          return 1;
-      } 
+      }
       else if(pid == 0) { //next child server
-         //server_main program launch
+         // send port to server and change port each time
          execlp("./server_main","server_main",NULL); //system call provided by Andrew
+      }
+      else {
+         wait(NULL);
+         printf("that's done");
       }
       sleep(1); //to give time for servers to open
    }
 
    //opening fifo to send names to servers
-   int fd = open("myfifo1", 01); //this waits until child server has opened fifo 
+   int fd = open("myfifo1", 01); //this waits until child server has opened fifo
       if (fd== -1){
       return 1;
    }
@@ -48,15 +55,14 @@ int main() {
    for (int i=0;i<3;i++){
       write(fd, &server_name, sizeof(int));
       server_name++; //incrementing name
+      printf("%d",server_name);
    }
 
-   /* parent process */
-	/* parent will wait for the child to complete */
 
    sleep(10); //for debugging to give time for child servers to print to terminal
    close(fd);
    wait(NULL);
-	printf("\nAll child processes complete, Server Driver will exit\n");
-	
+   printf("\nAll child processes complete, Server Driver will exit\n");
+
    return 0;
 }
--- server_main.c
+++ server_main.c
@@ -1,5 +1,5 @@
 //Andrew Ingle 04/08/2021 - Team I - Final Project
-//Has to be built but not run. 
+//Has to be built but not run.
 //This is the primary server program, will be executed when server_driver calls execpl
 
 
@@ -15,7 +15,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-int main() {
+int server_main() {
    printf("\nServer %d says hello\n",getpid());
 
    int fd = open("myfifo1", O_RDONLY); //fifo between manager and assistant
@@ -28,27 +28,33 @@ int main() {
    close(fd);
 
    if (server_name == 1){
+         printf("getting there!\n");
 
       char client_message[50]; //buffer for socket receive for testing
          /* creation of the socket */
       int server_socket;
       server_socket = socket(AF_INET, SOCK_STREAM, 0);
-      
+
       struct sockaddr_in address;
       address.sin_family = AF_INET;
-      address.sin_port = htons(8001); //for local connections
+      switch(server_name) {
+         case 1:
+            address.sin_port = htons(7400); //for local connections
+         case 2:
+            address.sin_port = htons(7401); //for local connections
+         case 3:
+            address.sin_port = htons(7402); //for local connections
+      }
+         printf("\nServer %d is alive and named!\n",server_name);
+
       address.sin_addr.s_addr = INADDR_ANY; //for local connetions
-      //for testing remote connections: these didn't work
-      //address.sin_port = htons(22); //ports csx0.cs.okstate
-      //address.sin_addr.s_addr = inet_addr("10.203.72.24");
-      //inet_pton(AF_INET, "10.203.72.24", &(address.sin_addr));
 
       bind(server_socket, (struct sockaddr*)&address,sizeof(address));
       listen(server_socket, 5);
 
       int client_socket;
       client_socket = accept(server_socket, NULL, NULL);
-      
+
       recv(client_socket,client_message,sizeof(client_message),0);
       printf("\nServer %d receives following message from client:\n", server_name);
       printf("%s",client_message);
