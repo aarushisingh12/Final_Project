d16dfa599afb5449b8215d9ee060e667163f81f0
AndrewIngle <drew.ingle@gmail.com>
2021-04-11 10:34:21 -0500

    added reservationPortal() func to receive and print from server and accept input and send to server. Added acceptReceipt() fun on client side

--- client_main.c
+++ client_main.c
@@ -10,6 +10,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+void reservationPortal(int);
+void acceptReceipt(int);
+
 int main() {
    printf("\nClient says hello\n"); //for debugging
 
@@ -51,25 +54,42 @@ int main() {
     }
     send(sock,client_message,sizeof(client_message),0); //a debug test
 
-//RECEIVE LOOP FROM SERVER 
+    reservationPortal(sock);
+
+    sleep(1);
+    close(sock);
+
+   return 0;
+}
+
+void reservationPortal(int sock){
+
+   int inputInt;
+   //char[STRING_BUFFER] inputString;
+
+   //RECEIVE LOOP FROM SERVER 
 //will be receiving fixed sized buffer strings from server until received string == "input"
    //while 1
          //buffer = receive(sock, messageFromServer, sizeof(buffer))
          //if buffer == "end"  //server send end message, indicating client sent exit program to main menu
                //close(sock);
                //return 0;
-         //if buffer == "input"  //server will send this message when input needed
-               //scanf(%s,input)
+         //if buffer == "needstring"  //server will send this message when input needed
+               //scanf(%s,inputString)
                //send(sock, input, sizeof(buffer))
+         //if buffer == "needint"
+               //scanf(%d,inputInt)
+               //send(sock,)
          //if buffer == "receipt"
-               //call makeReceipt() //function that opens file and prints recieved data (receipt) to
+               //call acceptReceipt() //function that opens file and prints recieved data (receipt) to
          //else //diplay server messages
             //printf("%s",buffer)
 
+}
 
+void acceptReceipt(int sock){
+   //called when client reads "receipt" string sent to buffer from server via tcp
+   //opens receipt file
+   //receives receipt strings from server and fprints to receipt file until "receiptend" read in buffer
 
-    sleep(1);
-    close(sock);
-
-   return 0;
 }
