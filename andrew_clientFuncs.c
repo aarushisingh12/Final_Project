e084b73b16c59271269e4403d40739e3778244dc
AndrewIngle <drew.ingle@gmail.com>
2021-04-13 11:18:40 -0500

    added debug print statements

--- andrew_clientFuncs.c
+++ andrew_clientFuncs.c
@@ -3,6 +3,7 @@
 
 
 void reservationPortal(int sock){
+      printf("\nclient's reservation portal called\n");
       
       int intBuffer = 0; //temp buffer to hold int from customer input to send to server
       //char inputString[STRING_BUFFER_MAX]; //temp string buffer to hold string from customer input to send to server
@@ -14,7 +15,7 @@ void reservationPortal(int sock){
       //if received message is "end" or "receipt" custom activity ensues
       while (1){
             recv(sock, stringBuffer, sizeof(stringBuffer),0); //receives next message from server, stores in fixed size buffer
-            
+
             if (strcmp(stringBuffer, "end") == 0){  //server send end message, indicating client sent exit program to main menu
                   printf("\nReceived end code from server.\n"); //gor debugging
                   sleep(1);
