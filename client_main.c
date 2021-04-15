55b12ae3d29e1c0615e731dc9e9059d51a737cf4
AndrewIngle <drew.ingle@gmail.com>
2021-04-15 11:45:59 -0500

    remote tcp connection implemented and tested from local computer to csx works

--- client_main.c
+++ client_main.c
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h> //for hstend struct to use gethostbyname func hostent.h_addr_list should have ip addresses
 
 #include "andrew_clientFuncs.h"
 
@@ -38,7 +39,8 @@ int main() {
    struct sockaddr_in server_address;
    server_address.sin_family = AF_INET;
 
-   // windows ports maybe: 7400,7401,7402
+   //windows ports maybe: 7400,7401,7402
+   //ports seem to work fine on csx or local linus installation
    switch(chosenServer) {
          case 1:
             server_address.sin_port = htons(8001); //for local connections
@@ -48,15 +50,18 @@ int main() {
             server_address.sin_port = htons(8003); //for local connections
    }
 
-   server_address.sin_addr.s_addr = INADDR_ANY; //for local //if remote would contain ip address of server in long format
+   server_address.sin_addr.s_addr = INADDR_ANY; //for local 
+   //server_address.sin_addr.s_addr = inet_addr("10.203.72.24"); //for when connecting to remote server ip for csx0.cs.okstate.edu
 
    //for connecting to receive from Server
    printf("\nclient going to attempt to connect\n");
-   int connection = connect(sock,(struct sockaddr *) &server_address,
-   sizeof(server_address));
+   int connection = connect(sock,(struct sockaddr *) &server_address, sizeof(server_address));
    if (connection == -1){
        printf("There is an issue with connection!!!\n");
+       close(sock);
+       return 0;
     }
+    printf("connected with server\n");
 
    printf("\nclient about to call reservation portal\n"); 
    reservationPortal(sock); //will close socket and exit from within
