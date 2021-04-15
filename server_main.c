621e3ef3fdf282e096fb75def881bc1498282fd2
AndrewIngle <drew.ingle@gmail.com>
2021-04-15 10:42:54 -0500

    updated comments on server_main

--- server_main.c
+++ server_main.c
@@ -24,17 +24,17 @@ int main() {
 
 //NEED TO ADD SHARED MEMORY ACCESS
 
-   printf("\nServer %d says hello\n",getpid());
+   printf("\nServer %d says hello\n",getpid()); //for debugging
 
    //TODO: Set up thread pool code
 
-   int fd = open("myfifo1", O_RDONLY); //fifo between manager and assistant
+   int fd = open("myfifo1", O_RDONLY); //fifo between server_driver and server_main
 
    char client_message[50]; //buffer for socket receive for testing
 
    int server_name;
-   //reading from pipe connected to managaer
-   read(fd,&server_name,sizeof(int));
+   //reading from pipe connected to parent process server_driver
+   read(fd,&server_name,sizeof(int)); //server now named either 1,2 or 3
 
    printf("\nServer %d is alive and named!\n",server_name);
    close(fd);
@@ -50,8 +50,10 @@ int main() {
    address.sin_family = AF_INET;
 
 
-   //address.sin_port = htons(8001); //for testing on local linux connections
+ 
    //windows ports maybe: 7400,7401,7402
+
+   //based on server_name, port is assigned
    switch(server_name) {
       case 1:
          address.sin_port = htons(8001); //for local connections
@@ -60,6 +62,8 @@ int main() {
       case 3:
          address.sin_port = htons(8003); //for local connections
    }
+
+   //for debugging. shutting down other unused servers so their ports are released
    if (server_name!=1){
       printf("\nserver %d exited\n",server_name);
       exit(0);
@@ -71,12 +75,17 @@ int main() {
 
    listen(server_socket, 5); //will update second number to reflect max number of customers allowed at a time
    printf("\nserver %d listening for clients\n",server_name);
+
+
    //will eventually need to do this within loop so next client can be accepted
    int client_socket;
    client_socket = accept(server_socket, NULL, NULL);
-   printf("\nserver %d about to call trainTicketMaster()\n",server_name);
+
+
+   printf("\nserver %d about to call trainTicketMaster()\n",server_name); //for debugging
    //will eventually assign thread to call this:
    trainTicketMaster(client_socket,server_name);
+
 
    //thread will return to pool when client exits program from menu
 
