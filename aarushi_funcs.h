e99999e7bd1ecb093ead4967fd38c832175c5b88
aarushisingh12 <71734806+aarushisingh12@users.noreply.github.com>
2021-04-16 10:26:07 -0500

    updating summary files

---
+++ aarushi_funcs.h
@@ -0,0 +1,21 @@
+#ifndef aarushi_funcs_h
+#define aarushi_funcs_h
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int writeToSummaryFile(nextCustomer,server_name);
+int displayTicketInfo(int ticketNumber) {
+
+
+#endif /* aarushi_funcs.h */
