6c5bfac1bf93c07805e85668160260a01d193eea
MaxwellMeckling <34351230+MaxwellMeckling@users.noreply.github.com>
2021-04-16 18:35:33 -0500

    Added some code for semaphores and shared memory
    Code is in a state for the progress report submission.

--- trainSeating.c
+++ trainSeating.c
@@ -5,6 +5,12 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ipc.h> 	// Used by IPC maechanisms: messages, shared memory and semaphores
+//#include <sys/shm.h>    // Possibly also for shared memory
+#include <sys/mman.h>   // Needed for shared memory
+//#include <sys/stat.h>   //I don't think this one helps
+#include <fcntl.h>      // Needed for shared memory
+#include <semaphore.h>  // Used for semaphores
 
 #include "trainSeating.h" //Include our own header file
 
@@ -30,6 +36,69 @@ void updateDays(availableSeats *day1, availableSeats *day2, int sizeOfSeatsArray
     }
 }
 
+sem_t mutex; 
+
+/*
+void semaphoreStuff() {
+    //wait
+    sem_wait(&mutex);
+    printf("\n entered semaphore");
+    
+    //do stuff in critical section
+    
+    
+    //signal
+    printf("\n exiting semaphore");
+    sem_post(&mutex);
+}
+*/
+
+/*
+//sets up shared memory for the available seats structs day1 and day2
+//returns a pointer to the shared memory object
+availableSeats setUpSharedMemory(availableSeats *day1, availableSeats *day2) {
+     availableSeats *ptr; //pointer to shared memory object
+    
+    // size (in bytes) of shared memory object
+    const int SIZE = sizeof(availableSeats) * 2; //Size of our struct * 2 so we can hold two days of entries
+    // name of the shared memory object
+    const char *name = "CS4323";
+    // shared memory file descriptor 
+    int shm_fd;
+    
+    // Create or open a shared memory object
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666); 
+    // Set the shared memory object size
+    ftruncate(shm_fd, SIZE);
+    // Map the shared memory object into the current address space
+    ptr = mmap(0, SIZE,  PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    
+    //store each newProduct into shared memory object 
+    *(ptr) = &day1;
+    *(ptr + 1) = &day2;
+    
+    // memory map the shared memory object
+    ptr = (availableSeats *) mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+    
+    randomstruct = *(ptr); //get the item from shared memory
+    
+    return ptr;
+}
+*/
+
+/*
+//function to be called that removes shared memory at the end of process runtime
+void removeSharedMemory(availableSeats *ptr) {
+    
+    // Unmap the shared memory
+    munmap(&ptr, SIZE);
+    // Close the shared memory object
+    close(shm_fd);
+    // Delete the shared memory object
+    shm_unlink(name);
+}
+*/
+
 int trainSeating() {
     availableSeats day1; //Struct for the first day
     availableSeats day2; //Struct for the second day
@@ -48,6 +117,8 @@ int trainSeating() {
     printf("%s\n", day1.date);
     printf("%s\n", day2.date);
     
+    //setUpSharedMemory(&day1, &day2);
+    
     //Display array for a day in a 3 x 9 format:
     //this will need to be sent to client over tcp socket
     //to become function displayAvailableSeats()
@@ -64,6 +135,10 @@ int trainSeating() {
     
     printf("%s\n", day1.date);
     printf("%s\n", day2.date);
+    
+    sem_init(&mutex, 0, 1); //initialize semaphore
+    
+    sem_destroy(&mutex); //destroy semaphore
     
     return 0;
 }
