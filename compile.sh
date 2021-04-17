ef9d8ea2683a5cd80ae6358ff9c163aae36420ae
Caleb Cunningham <cbcunningham20@gmail.com>
2021-04-17 14:40:38 -0500

    Added a bash script to create object files

---
+++ compile.sh
@@ -0,0 +1,3 @@
+gcc server_main.c trainTicketMaster.c server_tempFunctions.c caleb_server.c  -o server_main -lpthread
+gcc server_driver.c -o server_driver
+gcc client_main.c andrew_clientFuncs.c -o client_main
