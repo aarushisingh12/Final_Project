cc0d318367ca74d155d02510ec3969b3d5b7c27d
ThreeHoolagins <cbcunningham20@gmail.com>
2021-04-30 16:07:51 -0500

    updated compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
-gcc server_main.c trainTicketMaster.c server_tempFunctions.c caleb_server.c  -o server_main -lpthread
+gcc server_main.c trainTicketMaster.c server_tempFunctions.c caleb_server.c trainSeating.c aarushi_funcs.c -o server_main -lpthread -lrt
 gcc server_driver.c -o server_driver
 gcc client_main.c andrew_clientFuncs.c -o client_main
+
